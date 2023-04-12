9187e8d4c8de33932f1d63395e4d04ed647d64dd
xiaotan225 <2254301565@qq.com>
2023-04-12 14:30:12 +0800

    first commit

---
+++ 来源/default.ts
@@ -0,0 +1,12 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本站仅用于演示，填入自己的 key 才可使用。
+- [如果没有key可以点击访问](https://ai.shenqiwang.top) 欢迎您的到来！
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
